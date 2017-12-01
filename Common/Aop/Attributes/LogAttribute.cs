54f644c98bb07e90b4864f6b4f55a2507876fd4b
shichaohu <502242999@qq.com>
2017-12-01 10:17:05 +0800

    目录调整

---
+++ Common/Aop/Attributes/LogAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Aop.Attributes
+{
+    public sealed class LogAttribute: BaseAttribute
+    {
+        public override void OnExcuting(IInvocation invocation)
+        {
+            //"日志执行前"
+        }
+
+        public override void OnExited(IInvocation invocation)
+        {
+            //"日志执行后"
+        }
+    }
+}
